f8da02fc82476a47b3705470b1ae7ce18308cf13
Swesterl <swesterl@kth.se>
2016-10-10 09:53:45 +0200

    test

--- tictac3d/src/Player.java
+++ tictac3d/src/Player.java
@@ -10,72 +10,115 @@ public class Player {
      *            time before which we must have returned
      * @return the next state the board is in after our move
      */
+    public ArrayList<Integer> myAwesomeList;
+    public int maxDepth;
+
     public GameState play(final GameState gameState, final Deadline deadline) {
         Vector<GameState> nextStates = new Vector<GameState>();
         gameState.findPossibleMoves(nextStates);
 
 
         /*
-        if(nextStates.size() % 2 == 1 || nextStates.size() > 12){
+        if(nextStates.size() % 2 == 1 || nextStates.size() > 44){
             Random random = new Random();
             return nextStates.elementAt(random.nextInt(nextStates.size()));
         }
         */
-        
+
         if (nextStates.size() == 0) {
-            // Must play "pass" move if there are no other moves possible.
             return new GameState(gameState, new Move());
         }
 
-        int AorB = gameState.getNextPlayer();
-        int stupidInt;
-        boolean nextPlayer;
-
-        if (AorB == 1){
-            nextPlayer = false;
-            stupidInt = -1;
-        }
-        else {
-            nextPlayer = true;
-            stupidInt = 1;
-        }
 
         int[] moveValue = new int[nextStates.size()];
+        myAwesomeList = new ArrayList<Integer>();
         int alpha = -1000000;
         int beta = 1000000;
+        int maxDepth = 6;
 
-
-        int winOrLoseInRound = -1;
-        int bestChoice = 0;
-        int count = 0;
         for (GameState s : nextStates){
-            if (s.isEOG()) {
-                if (s.isXWin()) {
-                    winOrLoseInRound = count;
-                    break;
-                }
+            if (s.isXWin()) {
+                return s;
             }
-            //TODO break win and lose into two separate loops so we can break earlier!
+        }
+
+        for (GameState s : nextStates){
+            if(otk(s)){
+                return s;
+            }
+        }
+
+
+
+        int score2 = alphaBeta(gameState,maxDepth,1,alpha,beta,true);
+        return nextStates.elementAt(myAwesomeList.indexOf(score2));
+    }
+
+
+
+    public int alphaBeta(GameState state, int md, int depth, int alpha, int beta, boolean playerATurn) {
+        int v = -100000000;
+        if (state.isEOG()) {
+            //This is gamma
+            if (state.isXWin()) {
+                v = 5000;
+            }
+            else if (state.isOWin()) {
+                v =  -500;
+            }
             else {
-                if(otk(s)){
-                    winOrLoseInRound = count;
-                }
+                v = 0;
             }
-            count++;
         }
-        if (winOrLoseInRound == -1) {
-            Random random = new Random();
-            bestChoice = random.nextInt(nextStates.size());
+
+        else if(depth == md) {
+            int leafScore = eval5(state, playerATurn);
+            v = leafScore;
         }
+
         else {
-            bestChoice = winOrLoseInRound;
+
+            if (playerATurn) {
+                Vector<GameState> possibleStates = new Vector<GameState>();
+                state.findPossibleMoves(possibleStates);
+
+                v = -100000;
+
+                for (GameState rS : possibleStates){
+                    int childValue = alphaBeta(rS,md,depth + 1,alpha,beta,false);
+                    v = Math.max(v, childValue);
+
+                    if (depth == 1) {
+                        myAwesomeList.add(childValue);
+                    }
+
+                    alpha = Math.max(alpha,v);
+                    if (beta <= alpha){
+                        break;
+                    }
+                }
+
+            }
+            else {
+                Vector<GameState> possibleStates = new Vector<GameState>();
+                state.findPossibleMoves(possibleStates);
+
+                v = 100000;
+
+
+
+                for (GameState rS : possibleStates){
+                    v = Math.min(v, alphaBeta(rS,md,depth +1 ,alpha,beta,true));
+                    beta = Math.min(beta,v);
+                    if (beta <= alpha){
+                        break;
+                    }
+                }
+
+            }
         }
-        return nextStates.elementAt(bestChoice)
-;
 
-
-
-
+        return v;
     }
 
     public boolean otk(GameState s){
@@ -252,6 +295,181 @@ public class Player {
         }
 
         return score;
+    }
+
+    public int eval(GameState s, boolean pAt){
+
+        Move moveThatGotUsHere = s.getMove();
+        int indexOnBoard = Integer.parseInt(moveThatGotUsHere.toMessage().split("_")[1]);
+        int col = s.cellToCol(indexOnBoard);
+        int row = s.cellToRow(indexOnBoard);
+        int lay = s.cellToLay(indexOnBoard);
+
+
+        int sum = 0;
+        int constant = 0;
+        int antiConstant = 0;
+        int ab = 1;
+
+        if (pAt){
+            constant = Constants.CELL_X;
+            antiConstant = Constants.CELL_O;
+            ab = 1;
+        }
+        else {
+            constant = Constants.CELL_O;
+            antiConstant = Constants.CELL_X;
+            ab = -1;
+        }
+
+        sum += Math.pow(nrOfMarksOnRow(s, row, lay, constant),2);
+        sum += Math.pow(nrOfMarksOnCol(s, col, lay, constant),2);
+        sum += Math.pow(nrOfMarksOnLay(s, row, col, constant),2);
+        sum += Math.pow(nrOfMarksOnRowDia(s, row, col, lay, constant),2);
+        sum += Math.pow(nrOfMarksOnColDia(s, row, col, lay, constant),2);
+        sum += Math.pow(nrOfMarksOnLayDia(s, row, col, lay, constant),2);
+        sum += Math.pow(nrOfMarksOnDiaDia(s, row, col, lay, constant),2);
+
+        return sum*ab;
+    }
+
+    public int eval2(GameState s, boolean pAt){
+
+        Move moveThatGotUsHere = s.getMove();
+        int indexOnBoard = Integer.parseInt(moveThatGotUsHere.toMessage().split("_")[1]);
+        int col = s.cellToCol(indexOnBoard);
+        int row = s.cellToRow(indexOnBoard);
+        int lay = s.cellToLay(indexOnBoard);
+
+
+
+        int constant = 0;
+        int antiConstant = 0;
+        int ab = 1;
+
+
+        if (pAt){
+            constant = Constants.CELL_X;
+            antiConstant = Constants.CELL_O;
+            ab = 1;
+        }
+        else {
+            constant = Constants.CELL_O;
+            antiConstant = Constants.CELL_X;
+            ab = -1;
+        }
+
+        int score = 0;
+        for(int i  = 0 ; i < 4 ; i++) {
+            for(int j  = 0 ; j < 4 ; j++) {
+                for(int k  = 0 ; k < 4 ; k++) {
+                    int thingInCell = s.at(i,j,k);
+                    if(thingInCell == constant){
+                        score++;
+                    }
+                }
+            }
+        }
+
+        score = score * 3;
+
+        for(int i  = 0 ; i < 4 ; i++) {
+            score += nrOfMarksOnRowDia(s, i, 0, 0, constant);
+            score += nrOfMarksOnColDia(s, 0, i, 0, constant);
+            score += nrOfMarksOnLayDia(s, 0, 0, i, constant);
+            score += nrOfMarksOnRowDia(s, i, 0, 3, constant);
+            score += nrOfMarksOnColDia(s, 3, i, 0, constant);
+            score += nrOfMarksOnLayDia(s, 0, 3, i, constant);
+        }
+
+        score += 2*nrOfMarksOnDiaDia(s, 0, 0, 0, constant);
+        score += 2*nrOfMarksOnDiaDia(s, 3, 0, 0, constant);
+        score += 2*nrOfMarksOnDiaDia(s, 3, 3, 0, constant);
+        score += 2*nrOfMarksOnDiaDia(s, 0, 3, 0, constant);
+
+
+
+
+        return score*ab;
+    }
+
+    public int eval3(GameState s, boolean playerATurn){
+        int constant = 0;
+        int antiConstantEval3 = 0;
+        int multi = 1;
+        if (playerATurn){
+            constant = Constants.CELL_X;
+            antiConstantEval3 = Constants.CELL_O;
+            multi = 1;
+        }
+        else {
+            constant = Constants.CELL_O;
+            antiConstantEval3 = Constants.CELL_X;
+            multi = -1;
+        }
+        Move moveThatGotUsHere = s.getMove();
+        int indexOnBoard = Integer.parseInt(moveThatGotUsHere.toMessage().split("_")[1]);
+        int col = s.cellToCol(indexOnBoard);
+        int row = s.cellToRow(indexOnBoard);
+        int lay = s.cellToLay(indexOnBoard);
+        int sum = 0;
+        if(nrOfMarksOnRow(s, row, lay, constant) > 2){if(nrOfMarksOnRow(s, row, lay, antiConstantEval3) == 0){sum += 1;}}
+        if(nrOfMarksOnCol(s, col, lay, constant) > 2){if(nrOfMarksOnCol(s, row, lay, antiConstantEval3) == 0){sum += 1;}}
+        if(nrOfMarksOnLay(s, row, col, constant) > 2){if(nrOfMarksOnLay(s, row, lay, antiConstantEval3) == 0){sum += 1;}}
+        if(nrOfMarksOnRowDia(s, row, col, lay, constant) > 2){if(nrOfMarksOnRowDia(s, row, col, lay, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnColDia(s, row, col, lay, constant) > 2){if(nrOfMarksOnColDia(s, row, col, lay, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnLayDia(s, row, col, lay, constant) > 2){if(nrOfMarksOnLayDia(s, row, col, lay, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnDiaDia(s, row, col, lay, constant) > 2){if(nrOfMarksOnDiaDia(s, row, col, lay, antiConstantEval3) == 0){sum += 2;}}
+
+        return sum*multi;
+    }
+
+    public int eval4(GameState s, boolean playerATurn){
+        int constant = 0;
+        int antiConstantEval3 = 0;
+        int multi = 1;
+        int sum = 0;
+        if (playerATurn){
+            constant = Constants.CELL_X;
+            antiConstantEval3 = Constants.CELL_O;
+            multi = 1;
+        }
+        else {
+            constant = Constants.CELL_O;
+            antiConstantEval3 = Constants.CELL_X;
+            multi = -1;
+        }
+
+        for(int i  = 0 ; i < 4 ; i++) {
+            for(int j  = 0 ; j < 4 ; j++) {
+                if(nrOfMarksOnRow(s, i, j, constant) > 2){if(nrOfMarksOnRow(s, i, j, antiConstantEval3) == 0){sum += 1;}}
+                if(nrOfMarksOnCol(s, i, j, constant) > 2){if(nrOfMarksOnCol(s, i, j, antiConstantEval3) == 0){sum += 1;}}
+                if(nrOfMarksOnLay(s, i, j, constant) > 2){if(nrOfMarksOnLay(s, i, j, antiConstantEval3) == 0){sum += 1;}}
+
+            }
+            if(nrOfMarksOnRowDia(s, i, 0, 0, constant) > 2){if(nrOfMarksOnRowDia(s, i, 0, 0, constant) == 0){sum += 1;}}
+            if(nrOfMarksOnColDia(s, 0, i, 0, constant) > 2){if(nrOfMarksOnColDia(s, 0, i, 0, constant) == 0){sum += 1;}}
+            if(nrOfMarksOnLayDia(s, 0, 0, i, constant) > 2){if(nrOfMarksOnLayDia(s, 0, 0, i, constant) == 0){sum += 1;}}
+            if(nrOfMarksOnRowDia(s, i, 0, 3, constant) > 2){if(nrOfMarksOnRowDia(s, i, 0, 3, constant) == 0){sum += 1;}}
+            if(nrOfMarksOnColDia(s, 3, i, 0, constant) > 2){if(nrOfMarksOnColDia(s, 3, i, 0, constant) == 0){sum += 1;}}
+            if(nrOfMarksOnLayDia(s, 0, 3, i, constant) > 2){if(nrOfMarksOnLayDia(s, 0, 3, i, constant) == 0){sum += 1;}}
+        }
+
+        if(nrOfMarksOnDiaDia(s, 0, 0, 0, constant) > 2){if(nrOfMarksOnDiaDia(s, 0, 0, 0, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnDiaDia(s, 3, 0, 0, constant) > 2){if(nrOfMarksOnDiaDia(s, 3, 0, 0, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnDiaDia(s, 3, 3, 0, constant) > 2){if(nrOfMarksOnDiaDia(s, 3, 3, 0, antiConstantEval3) == 0){sum += 2;}}
+        if(nrOfMarksOnDiaDia(s, 0, 3, 0, constant) > 2){if(nrOfMarksOnDiaDia(s, 0, 3, 0, antiConstantEval3) == 0){sum += 2;}}
+
+        return sum*multi;
+    }
+
+    public int eval5(GameState s, boolean playerATurn){
+        if (playerATurn){
+            return 2*eval4(s,true) + eval4(s,false);
+        }
+        else {
+            return eval4(s,true) + 2*eval4(s,false);
+        }
     }
 
 }
